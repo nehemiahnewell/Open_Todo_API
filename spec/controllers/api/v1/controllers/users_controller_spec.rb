35c56b2b91e946d38cf6616cac5e018f5ca955a9
Conrad Taylor <conradwt@gmail.com>
2016-04-27 13:46:53 -0700

    Fix - UsersController#index specs.

--- spec/controllers/api/v1/controllers/users_controller_spec.rb
+++ spec/controllers/api/v1/controllers/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
-  # let(:my_user) { FactoryGirl.create(:user, username: "fake", password: "faker") }
+  let(:my_user) { FactoryGirl.create(:user, username: "fake", password: "faker") }
  
   context "unauthenticated users" do
     it "GET index returns Access denied." do
@@ -12,14 +12,14 @@ RSpec.describe Api::V1::UsersController, type: :controller do
  
   context "authenticated users" do
     before do
-      User.find_or_create_by!(username: "fake", password: "faker")
+      # User.find_or_create_by!(username: "fake", password: "faker")
       basic = ActionController::HttpAuthentication::Basic
       @credentials = basic.encode_credentials( my_user.username, my_user.password )
       request.env['HTTP_AUTHORIZATION'] = @credentials
     end
     
     it "GET /api/v1/users" do
-      get :index
+      get :index, {}
       expect( response.status ).to eq( 200 )
       expect( response.content_type ).to eq( Mime::JSON )
     end
