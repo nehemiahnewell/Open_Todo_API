7618969700d5c346aecbdadd2563762920273499
nehemiahnewell <nehemiah.newell@gmail.com>
2016-04-27 04:39:09 +0000

    Api start

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
+  
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users
+    end
+  end
 end
